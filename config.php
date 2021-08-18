f9fbfadce2f08dad085f3d55ab4ce3b024c2f0f8
Americium <vadim.4392@mail.ru>
2021-08-18 23:08:28 +0300

    create project structure and settings

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('Access_denied');
+
+const SITE_URL = 'http://shop';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'shop';
---
+++ core/base/settings/internal_settings.php
@@ -0,0 +1,23 @@
+<?php
+defined('VG_ACCESS') or die('Access_denied');
+
+const TEMPLATE = 'templates/default/';
+const ADMIN_TAMPLATES = 'core/admin/views/';
+
+const COOKIE_VSERSION = '1.0.0';
+const CRYPT_KEY = '';
+const COOKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const OTY = 8;
+const OTY_LINKS = 3;
+
+const ADMIN_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
+
+const USER_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
